d88a76a57d6d8ce552730cdb39f617319d70e4dd
IT22323316 <it22323316@my.sliit.lk>
2025-04-26 12:03:46 +0530

    luvi

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -24,7 +24,7 @@ export default function Login() {
     setLoading(true);
     setError(null);
     try {
-      const { data } = await axios.post("http://localhost:5559/users/login", {
+      const { data } = await axios.post("http://localhost:5000/auth/register", {
         email,
         password
       });
